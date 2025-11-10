9a3091a342929fb185abb0fbb346da0aff632f6b
2over12 <ismith@galois.com>
2025-11-10 16:58:59 -0500

    parse header

--- src/main/java/cartographer/GREASECovJson.java
+++ src/main/java/cartographer/GREASECovJson.java
@@ -1,10 +1,16 @@
 package cartographer;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.file.AccessMode;
 import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
 
+import ghidra.app.util.bin.FileByteProvider;
+import ghidra.app.util.bin.format.elf.ElfException;
+import ghidra.app.util.bin.format.elf.ElfHeader;
 import ghidra.program.model.listing.Program;
 import ghidra.program.model.mem.MemoryBlock;
 import tools.jackson.core.type.TypeReference;
@@ -13,8 +19,37 @@ import tools.jackson.databind.ObjectMapper;
 public class GREASECovJson {
     private HashMap<Long, TreeMap<Long, MemoryBlock>> regionToBlockOffset;
 
-    public GREASECovJson(String section_infos, Program prog) {
-        parseSectionInfos(section_infos);
+    private static HashMap<Long, String> sectionNameToIndex(Program prog) throws IOException, ElfException {
+        var pth = prog.getExecutablePath();
+        // TODO: There ought to be a way to get the FSRL
+        var flBts = new FileByteProvider(new File(pth), null, AccessMode.READ);
+        var header = new ElfHeader(flBts, null);
+        HashMap<Long, String> mp = new HashMap<>();
+        Long ind = 0L;
+        for (var sec : header.getSections()) {
+            mp.put(ind++, sec.getNameAsString());
+        }
+
+        return mp;
+    }
+
+    public GREASECovJson(String section_infos, Program prog) throws IOException, ElfException {
+        var sections = parseSectionInfos(section_infos);
+        // TODO: this relies on looking up block by name matching the section name which
+        // is usually true
+        // but we should use the byte source + section offset to do a more precise
+        // mapping to the loaded memory block
+        var sectionIndexToName = sectionNameToIndex(prog);
+        for (var sinfo : sections) {
+            if (sectionIndexToName.containsKey(sinfo.section_index)) {
+                var targetSectionName = sectionIndexToName.get(sinfo.section_index);
+                var potentialBlock = prog.getMemory().getBlock(targetSectionName);
+                if (potentialBlock != null) {
+                    var offMap = regionToBlockOffset.putIfAbsent(sinfo.section_mem_addr.region_index, new TreeMap<>());
+                    offMap.put(sinfo.section_mem_addr.region_offset, potentialBlock);
+                }
+            }
+        }
     }
 
     public static class GREASEMemAddr {
